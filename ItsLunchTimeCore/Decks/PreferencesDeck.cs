6e67119746dda109a23681ca17f86bfeb14c6809
Bruno Guedes <bsguedes@inf.ufrgs.br>
2017-10-05 21:35:15 -0300

    Added 30 preference cards

--- ItsLunchTimeCore/Decks/PreferencesDeck.cs
+++ ItsLunchTimeCore/Decks/PreferencesDeck.cs
@@ -11,7 +11,32 @@ namespace ItsLunchTimeCore.Decks
                 new PreferenceCard(Restaurant.Russo, 5, Restaurant.Panorama, 1, Restaurant.Silva, Punishment.TwoVP, Punishment.TwoVP),
                 new PreferenceCard(Restaurant.Russo, 4, Restaurant.Silva, 3, Restaurant.Palatus, Punishment.TwoVP, Punishment.OneVP),
                 new PreferenceCard(Restaurant.Russo, 4, Restaurant.GustoDiBacio, 2, Restaurant.Silva, Punishment.OneVP, Punishment.OneVP),
-                new PreferenceCard(Restaurant.Russo, 4, Restaurant.Palatus, 2, Restaurant.GustoDiBacio, Punishment.TwoVP, Punishment.TwoVP)
+                new PreferenceCard(Restaurant.Russo, 4, Restaurant.Palatus, 2, Restaurant.GustoDiBacio, Punishment.TwoVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.Palatus, 6, Restaurant.JoeAndLeos, 3, Restaurant.Panorama, Punishment.OneVP, Punishment.OneDollar),
+                new PreferenceCard(Restaurant.Palatus, 5, Restaurant.Panorama, 4, Restaurant.JoeAndLeos, Punishment.OneVP, Punishment.OneDollar),
+                new PreferenceCard(Restaurant.Palatus, 5, Restaurant.Silva, 1, Restaurant.Russo, Punishment.TwoVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.Palatus, 4, Restaurant.GustoDiBacio, 3, Restaurant.Silva, Punishment.OneVP, Punishment.OneVP),
+                new PreferenceCard(Restaurant.Palatus, 4, Restaurant.Russo, 2, Restaurant.Silva, Punishment.OneVP, Punishment.OneVP),
+                new PreferenceCard(Restaurant.GustoDiBacio, 6, Restaurant.JoeAndLeos, 1, Restaurant.Silva, Punishment.OneVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.GustoDiBacio, 6, Restaurant.Panorama, 4, Restaurant.JoeAndLeos, Punishment.OneVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.GustoDiBacio, 5, Restaurant.Silva, 3, Restaurant.Palatus, Punishment.OneVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.GustoDiBacio, 5, Restaurant.Palatus, 2, Restaurant.Russo, Punishment.OneVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.GustoDiBacio, 4, Restaurant.Russo, 2, Restaurant.Palatus, Punishment.OneDollar, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.Silva, 7, Restaurant.JoeAndLeos, 4, Restaurant.Palatus, Punishment.TwoVP, Punishment.OneDollar),
+                new PreferenceCard(Restaurant.Silva, 6, Restaurant.Panorama, 3, Restaurant.GustoDiBacio, Punishment.OneVP, Punishment.OneVP),
+                new PreferenceCard(Restaurant.Silva, 6, Restaurant.GustoDiBacio, 3, Restaurant.Panorama, Punishment.OneVP, Punishment.OneDollar),
+                new PreferenceCard(Restaurant.Silva, 5, Restaurant.Palatus, 1, Restaurant.JoeAndLeos, Punishment.OneVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.Silva, 5, Restaurant.Russo, 2, Restaurant.Panorama, Punishment.OneVP, Punishment.OneVP),
+                new PreferenceCard(Restaurant.Panorama, 7, Restaurant.JoeAndLeos, 3, Restaurant.Russo, Punishment.OneVP, Punishment.OneDollar ),
+                new PreferenceCard(Restaurant.Panorama, 7, Restaurant.Silva, 3, Restaurant.Russo, Punishment.TwoVP, Punishment.OneDollar ),
+                new PreferenceCard(Restaurant.Panorama, 6, Restaurant.GustoDiBacio, 4, Restaurant.JoeAndLeos, Punishment.OneVP, Punishment.OneVP ),
+                new PreferenceCard(Restaurant.Panorama, 6, Restaurant.Palatus, 1, Restaurant.JoeAndLeos, Punishment.OneDollar, Punishment.TwoVP ),
+                new PreferenceCard(Restaurant.Panorama, 5, Restaurant.Russo, 2, Restaurant.JoeAndLeos, Punishment.OneVP, Punishment.TwoVP ),
+                new PreferenceCard(Restaurant.JoeAndLeos, 7, Restaurant.Panorama, 4, Restaurant.GustoDiBacio, Punishment.OneVP, Punishment.OneVP),
+                new PreferenceCard(Restaurant.JoeAndLeos, 7, Restaurant.Silva, 4, Restaurant.Panorama, Punishment.OneVP, Punishment.OneDollar),
+                new PreferenceCard(Restaurant.JoeAndLeos, 7, Restaurant.GustoDiBacio, 1, Restaurant.Russo, Punishment.OneVP, Punishment.TwoVP),
+                new PreferenceCard(Restaurant.JoeAndLeos, 6, Restaurant.Palatus, 3, Restaurant.GustoDiBacio, Punishment.OneDollar, Punishment.OneVP),
+                new PreferenceCard(Restaurant.JoeAndLeos, 6, Restaurant.Russo, 2, Restaurant.Palatus, Punishment.OneDollar, Punishment.OneVP),
             };
         }
     }
@@ -40,10 +65,10 @@ namespace ItsLunchTimeCore.Decks
 
     public class Punishment
     {
-        public static readonly Punishment OneVP = new Punishment(PunishmentType.VictoryPoints, 1);
-        public static readonly Punishment TwoVP = new Punishment(PunishmentType.VictoryPoints, 2);
-        public static readonly Punishment OneDollar = new Punishment(PunishmentType.Cash, 1);
-        public static readonly Punishment TwoDollar = new Punishment(PunishmentType.Cash, 2);
+        public static readonly Punishment OneVP = new Punishment(PunishmentType.VictoryPoints, -1);
+        public static readonly Punishment TwoVP = new Punishment(PunishmentType.VictoryPoints, -2);
+        public static readonly Punishment OneDollar = new Punishment(PunishmentType.Cash, -1);
+        public static readonly Punishment TwoDollar = new Punishment(PunishmentType.Cash, -2);
 
         public PunishmentType Type { get; }
         public int Value { get; }
